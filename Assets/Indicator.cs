61c46df3ebd2ac35a93826c9931a5f799a95ce63
Keijiro Takahashi <keijiro@gmail.com>
2024-10-29 17:18:52 +0900

    better animation

--- Assets/Indicator.cs
+++ Assets/Indicator.cs
@@ -3,13 +3,14 @@ using Unity.Mathematics;
 
 public sealed class Indicator : MonoBehaviour
 {
-    [field:SerializeField] float3 Frequency { get; set; }
-    [field:SerializeField] float3 Amplitude { get; set; }
+    [field:SerializeField] float Frequency { get; set; } = 1;
 
     void Update()
     {
-        var v = math.sin(Frequency * Time.time) * Amplitude;
-        transform.localPosition = math.float3(v.xy, 0);
-        transform.localRotation = quaternion.RotateZ(v.z);
+        var t = math.frac(Time.time * Frequency);
+        var y = 1 - math.pow(t * 2 - 1, 2);
+        var r = math.smoothstep(0, 1, t) * 2 * math.PI;
+        transform.localPosition = math.float3(0, y, 0);
+        transform.localRotation = quaternion.RotateZ(r);
     }
 }
